38fec1d1444376ed88cc210185ec3fa1bd877337
pgattic <pgattic@gmail.com>
2023-10-13 07:48:07 -0600

    improve code readability, use stderr for errors

--- brainfetch.c
+++ brainfetch.c
@@ -5,10 +5,10 @@
 
 		BRAINFETCH
 			A BrainF*** interpreter/debugger written in standard C
-			By Preston Corless
+			By Preston Corless (pgattic)
 
 		Usage:
-			brainfetch [filename].bf [-d]
+			brainfetch [FILE].bf [-d]
 
 		Notes:
 		  - For security purposes, this interpreter has static Pogram and Work
@@ -26,6 +26,11 @@
 #define PRGMEM 50000	// Amount of bytes to reserve for program code buffer (code filesize limit for debug mode, else instruction count limit)
 #define WORKMEM 30000	// Amount of bytes to reserve for work ram
 
+void printHelp(char* arg) {
+	printf("Usage:\n  %s [FILE].bf [OPTIONS]\n", arg);
+	printf("Options:\n  -d	debug mode\n");
+}
+
 int loadCode(FILE* f, char* dest, char debugMode) {
 	char ch;
 	int head = 0;
@@ -35,8 +40,8 @@ int loadCode(FILE* f, char* dest, char debugMode) {
 			dest[head] = ch;
 			head++;
 			if (head >= PRGMEM) {
-				printf("\nERROR: Out of Program Memory. Program file too large.\nSee \"Notes\" in brainfetch.c.\n");
-				return 0;
+				fprintf(stderr, "\nERROR: Out of Program Memory. Program file too large.\nSee \"Notes\" in brainfetch.c.\n");
+				return -1;
 			}
 		}
 	} while (ch != EOF);
@@ -47,7 +52,7 @@ void printLinePosition(char* code, int pos) {
 	int li = 1;
 	int ch = 1;
 	for (int i = 0; i < pos; i++) {
-		if (code[i] == 10) {	// LF (newline) character
+		if (code[i] == '\n') {
 			li++;
 			ch = 1;
 		}
@@ -62,13 +67,13 @@ int testCode(char* prg, int size) {	// test code for issues with brackets (quick
 		if (prg[i] == '[') { bracketBalance++; }
 		if (prg[i] == ']') { bracketBalance--; }
 		if (bracketBalance < 0) {
-			printf("\nERROR: Unopened \"]\" (\"[\" expected)\n");
+			fprintf(stderr, "\nERROR: Unopened \"]\" (\"[\" expected)\n");
 			printLinePosition(prg, i);
 			return 0;
 		}
 	}
 	if (bracketBalance) {
-		printf("\nERROR: Unclosed \"[\" (\"]\" expected)\n");
+		fprintf(stderr, "\nERROR: Unclosed \"[\" (\"]\" expected)\n");
 	}
 	return !bracketBalance;
 }
@@ -90,21 +95,27 @@ void memDump(char* prg, char* mem, int head, int ptr) {	// Simple memory dump
 
 int main(int argc, char** argv) {
 
-	if (argc < 2) {				// end program if no file specified/found
-		printf("Usage: %s [code].bf\n", argv[0]);
+	if (argc < 2) {				// end program if no file specified
+		printHelp(argv[0]);
 		return 1;
 	}
 
 	FILE* f = fopen(argv[1], "r");	// open the file specified as an argument into the program
 
+	if (!f) {					// end program if file not found
+		fprintf(stderr,"%s: no such file\n\n", argv[1]);
+		printHelp(argv[0]);
+		return 1;
+	}
+
 	char debugMode = argv[2] ? (argv[2][0] == '-' && argv[2][1] == 'd' && argv[2][2] == 0) : 0;	// curses, C string comparison!
 
 	char prg[PRGMEM];		// array that the bf code is temporarily stored in (for easy access by interpreter)
 	int head = 0;			// index of prg that instructions are being read from
-	int fileSize = loadCode(f, prg, debugMode);
+	int fileSize = loadCode(f, prg, debugMode); // fileSize is actually the index of the last byte, or the file's size minus one.
 
-	if (!fileSize || !testCode(prg, fileSize)) {	// run tests, exit program if failed
-		return 0;
+	if (fileSize < 0 || !testCode(prg, fileSize)) {	// run tests, exit program if failed
+		return 1;
 	}
 
 	char mem[WORKMEM] = {0};	// Brainfetch "memory"
@@ -122,13 +133,13 @@ int main(int argc, char** argv) {
 				mem[ptr] = getchar();
 				break;
 			case '[':
-				if (!mem[ptr]) {				// check for nonzero value
-					int bracketBalance = 0;		// positive: outstanding open brackets; negative: outstanding close brackets
+				if (mem[ptr] == 0) {
+					int bracketBalance = 1;		// positive: outstanding open brackets; zero: all opened brackets are accounted for
 					do {
 						head++;
 						if (prg[head] == '[') { bracketBalance++; }
 						if (prg[head] == ']') { bracketBalance--; }
-					} while (bracketBalance >= 0);
+					} while (bracketBalance > 0);
 				}
 				break;
 			case ']':
@@ -147,12 +158,13 @@ int main(int argc, char** argv) {
 				break;
 		}
 		if (ptr < 0) {
-			printf("\nERROR: Pointer underflow (Memory pointer < 0)\n");
+			fprintf(stderr, "\nERROR: Pointer underflow (Memory pointer < 0)\n");
 			printLinePosition(mem, head);
 			memDump(prg, mem, head, ptr);
-			return 0;
+			return 1;
 		} else if (ptr >= WORKMEM) {
-			printf("\nERROR: Out of Work Memory.\nSee \"Notes\" in brainfetch.c.\n");
+			fprintf(stderr, "\nERROR: Out of Work Memory.\nSee \"Notes\" in brainfetch.c.\n");
+			return 1;
 		}
 		head++;
 	}
