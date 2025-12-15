f2368b4081d459b4a1b66ecb990c9bf6ef14472c
pgattic <pgattic@gmail.com>
2025-12-15 15:09:07 -0700

    [Rocq] Add optimization functions and notation

--- interpreters/rocq/bf_convert.sh
+++ interpreters/rocq/bf_convert.sh
@@ -1,16 +1,47 @@
 #!/usr/bin/env bash
-# usage: ./bf_convert.sh program.bf
+#
+# bf2coq.sh - Convert a Brainfuck program into Coq bf_program notation
+# using:
+#   - bf_program as a tree: bf_nil / bf_cons / bf_loop
+#   - custom entry syntax like: <[ + + [ - ! ] > . ! ]>
+# where "!" denotes bf_nil.
+#
+# Usage:
+#   ./bf2coq.sh '++[->+<]>.'
+#   echo '++[->+<]>.' | ./bf2coq.sh
 
-tr -cd '+<>.,[]-' < "$1" | sed '
-  s/+/bf_inc; /g
-  s/-/bf_dec; /g
-  s/>/bf_next; /g
-  s/</bf_prev; /g
-  s/\./bf_output; /g
-  s/,/bf_input; /g
-  s/\[/bf_loop [; /g
-  s/\]/]; /g
-  s/\[; /[/g
-  s/; ]/]/g
-  s/; $//
-'
+# Read input: from first argument if given, otherwise from stdin
+if [ -n "$1" ]; then
+  raw="$1"
+else
+  raw="$(cat)"
+fi
+
+# Keep only valid Brainfuck characters
+bf=$(printf "%s" "$raw" | tr -d -c '+-<>[],.')
+
+out=""
+
+# Process each character and map to Coq tokens
+#   + - > < , . stay the same (just spaced)
+#   [ becomes " ["
+#   ] becomes " ! ]"  (insert loop body terminator before each closing bracket)
+for ((i=0; i<${#bf}; i++)); do
+  ch=${bf:i:1}
+  case "$ch" in
+    '+') out+=" +" ;;
+    '-') out+=" -" ;;
+    '>') out+=" >" ;;
+    '<') out+=" <" ;;
+    ',') out+=" ," ;;
+    '.') out+=" @" ;;
+    '[') out+=" [" ;;
+    ']') out+=" ! ]" ;;
+  esac
+done
+
+# Add final terminator for the whole program
+out+=" !"
+
+# Wrap in the Coq bf_program notation
+printf "<[%s ]>\n" "$out"
--- interpreters/rocq/src/bf.v
+++ interpreters/rocq/src/bf.v
@@ -1,16 +1,17 @@
 From Stdlib Require Import List Arith.
-Import ListNotations.
 
-Inductive bf_command : Type :=
+Inductive bf_program : Type :=
+| bf_nil : bf_program
+| bf_cons : bf_command -> bf_program -> bf_program
+
+with bf_command : Type :=
 | bf_inc
 | bf_dec
 | bf_next
 | bf_prev
 | bf_input
 | bf_output
-| bf_loop (loop : list bf_command).
-
-Definition bf_program := list bf_command.
+| bf_loop (body : bf_program).
 
 Record bf_tape : Type := {
   left : list nat;
@@ -32,18 +33,6 @@ Definition bf_default_state : bf_state := {|
 
 (* Interpretation Commands *)
 
-Definition move_left (tape : bf_tape) : bf_tape :=
-  match left tape with
-  | nil => {| left := nil; curr := 0; right := (curr tape) :: (right tape) |}
-  | h :: t => {| left := t; curr := h; right := (curr tape) :: (right tape) |}
-  end.
-
-Definition move_right (tape : bf_tape) : bf_tape :=
-  match right tape with
-  | nil => {| left := (curr tape) :: (left tape); curr := 0; right := nil |}
-  | h :: t => {| left := (curr tape) :: (left tape); curr := h; right := t |}
-  end.
-
 Definition inc_cell (t : bf_tape) : bf_tape := {|
   left := left t;
   curr := S (curr t) mod 256;
@@ -56,8 +45,20 @@ Definition dec_cell (t : bf_tape) : bf_tape := {|
   right := right t;
 |}.
 
+Definition shift_left (tape : bf_tape) : bf_tape :=
+  match left tape with
+  | nil => {| left := nil; curr := 0; right := (curr tape) :: (right tape) |}
+  | h :: t => {| left := t; curr := h; right := (curr tape) :: (right tape) |}
+  end.
+
+Definition shift_right (tape : bf_tape) : bf_tape :=
+  match right tape with
+  | nil => {| left := (curr tape) :: (left tape); curr := 0; right := nil |}
+  | h :: t => {| left := (curr tape) :: (left tape); curr := h; right := t |}
+  end.
+
 (* Puts the head value of the input list into the current cell *)
-Definition input_cell (s : bf_state) : bf_state := 
+Definition input_cell (s : bf_state) : bf_state :=
   match input s with
   | nil => s
   | h :: t => {|
@@ -74,14 +75,12 @@ Definition output_cell (s : bf_state) : bf_state := {|
   output := curr (tape s) :: output s
 |}.
 
-Definition bf_default_fuel := 5000.
-
 Fixpoint interp (fuel : nat) (s : bf_state) (p : bf_program) : option bf_state :=
   match fuel, p with
-  | O, nil => Some s
+  | O, bf_nil => Some s
   | O, _ => None
-  | _, nil => Some s
-  | S (fuel'), h :: t => match interp_single fuel' s h with
+  | _, bf_nil => Some s
+  | S (fuel'), bf_cons h t => match interp_single fuel' s h with
     | Some s' => interp fuel' s' t
     | None => None
     end
@@ -91,8 +90,8 @@ with interp_single (loop_fuel : nat) (s : bf_state) (c : bf_command) : option bf
   match c with
   | bf_inc => Some {| tape := inc_cell (tape s); input := input s; output := output s |}
   | bf_dec => Some {| tape := dec_cell (tape s); input := input s; output := output s |}
-  | bf_next => Some {| tape := move_right (tape s); input := input s; output := output s |}
-  | bf_prev => Some {| tape := move_left (tape s); input := input s; output := output s |}
+  | bf_next => Some {| tape := shift_right (tape s); input := input s; output := output s |}
+  | bf_prev => Some {| tape := shift_left (tape s); input := input s; output := output s |}
   | bf_input => Some (input_cell s)
   | bf_output => Some (output_cell s)
   | bf_loop body => let fix loop (fuel : nat) (s : bf_state) := match fuel with
@@ -106,7 +105,10 @@ with interp_single (loop_fuel : nat) (s : bf_state) (c : bf_command) : option bf
     in loop loop_fuel s
   end.
 
-Definition default_interp := interp bf_default_fuel bf_default_state.
+Definition default_fuel := 5000.
+
+(* Helper functions *)
+Definition default_interp := interp default_fuel bf_default_state.
 
 Definition get_tape (state : option bf_state) : option bf_tape :=
   match state with
@@ -120,19 +122,53 @@ Definition get_curr (state : option bf_state) : option nat :=
   | None => None
   end.
 
+
+(* Notation *)
+
+Declare Custom Entry bf.
+
+(* Write BF programs as: <[ ... ]> *)
+Notation "<[ p ]>" := p
+  (p custom bf at level 0).
+
+(* End-of-program marker *)
+Notation "!" := bf_nil
+  (in custom bf at level 0).
+
+(* Sequencing: "cmd rest" means bf_cons cmd rest *)
+Notation "c p" := (bf_cons c p)
+  (in custom bf at level 0,
+      right associativity).
+
+(* Basic commands (BF-style) *)
+Notation "+" := bf_inc    (in custom bf at level 0).
+Notation "-" := bf_dec    (in custom bf at level 0).
+Notation ">" := bf_next   (in custom bf at level 0).
+Notation "<" := bf_prev   (in custom bf at level 0).
+Notation "," := bf_input  (in custom bf at level 0).
+
+(* Output command: use '@' instead of '.' to avoid conflicts *)
+Notation "@" := bf_output (in custom bf at level 0).
+
+(* Loops: [ body ] rest *)
+Notation "[ p ']' q" :=
+  (bf_cons (bf_loop p) q)
+  (in custom bf at level 0,
+      right associativity).
+
+
 (* Tests *)
 
 Example inc_test :
-  get_curr (default_interp [bf_inc]) = Some 1.
+  get_curr (default_interp <[ + !]>) = Some 1.
 Proof. reflexivity. Qed.
 
 Example quadruple_inc_test :
-  get_curr (default_interp [bf_inc; bf_inc; bf_inc; bf_inc]) = Some 4.
+  get_curr (default_interp <[ + + + + !]>) = Some 4.
 Proof. reflexivity. Qed.
 
-Definition hello_world : bf_program := [
-  bf_inc; bf_inc; bf_inc; bf_inc; bf_inc; bf_inc; bf_inc; bf_inc; bf_loop [bf_next; bf_inc; bf_inc; bf_inc; bf_inc; bf_loop [bf_next; bf_inc; bf_inc; bf_next; bf_inc; bf_inc; bf_inc; bf_next; bf_inc; bf_inc; bf_inc; bf_next; bf_inc; bf_prev; bf_prev; bf_prev; bf_prev; bf_dec]; bf_next; bf_inc; bf_next; bf_inc; bf_next; bf_dec; bf_next; bf_next; bf_inc; bf_loop [bf_prev]; bf_prev; bf_dec]; bf_next; bf_next; bf_output; bf_next; bf_dec; bf_dec; bf_dec; bf_output; bf_inc; bf_inc; bf_inc; bf_inc; bf_inc; bf_inc; bf_inc; bf_output; bf_output; bf_inc; bf_inc; bf_inc; bf_output; bf_next; bf_next; bf_output; bf_prev; bf_dec; bf_output; bf_prev; bf_output; bf_inc; bf_inc; bf_inc; bf_output; bf_dec; bf_dec; bf_dec; bf_dec; bf_dec; bf_dec; bf_output; bf_dec; bf_dec; bf_dec; bf_dec; bf_dec; bf_dec; bf_dec; bf_dec; bf_output; bf_next; bf_next; bf_inc; bf_output; bf_next; bf_inc; bf_inc; bf_output
-].
+Definition hello_world : bf_program :=
+  <[ + + + + + + + + [ > + + + + [ > + + > + + + > + + + > + < < < < - ! ] > + > + > - > > + [ < ! ] < - ! ] > > @ > - - - @ + + + + + + + @ @ + + + @ > > @ < - @ < @ + + + @ - - - - - - @ - - - - - - - - @ > > + @ > + + @ ! ]>.
 
 From Stdlib Require Import Ascii String List.
 
@@ -145,6 +181,129 @@ Definition output_text (state : option bf_state) : option string :=
 (* Works!!! *)
 Eval compute in output_text (default_interp hello_world).
 
+
+(* Optimization *)
+
+Inductive bf_opt_program : Type :=
+| bf_opt_nil : bf_opt_program
+| bf_opt_cons : bf_opt_command -> bf_opt_program -> bf_opt_program
+
+with bf_opt_command : Type :=
+| bf_opt_inc (count : nat)
+| bf_opt_dec (count : nat)
+| bf_opt_zero (* Meant for instances of `[-]` *)
+| bf_opt_next (count : nat)
+| bf_opt_prev (count : nat)
+| bf_opt_input
+| bf_opt_output
+| bf_opt_loop (body : bf_opt_program).
+
+Definition merge_command (cmd : bf_opt_command) (rest : bf_opt_program) : bf_opt_program :=
+  match cmd, rest with
+  | bf_opt_inc n,  bf_opt_cons (bf_opt_inc m) t =>  bf_opt_cons (bf_opt_inc (n + m)) t
+  | bf_opt_dec n,  bf_opt_cons (bf_opt_dec m) t =>  bf_opt_cons (bf_opt_dec (n + m)) t
+  | bf_opt_next n, bf_opt_cons (bf_opt_next m) t => bf_opt_cons (bf_opt_next (n + m)) t
+  | bf_opt_prev n, bf_opt_cons (bf_opt_prev m) t => bf_opt_cons (bf_opt_prev (n + m)) t
+  | _, _ => bf_opt_cons cmd rest
+  end.
+
+Fixpoint bf_optimize (p : bf_program) : bf_opt_program :=
+  match p with
+  | bf_nil => bf_opt_nil
+  | bf_cons c t =>
+      (* optimize a single command, using bf_optimize recursively for loops *)
+      let optimized_c :=
+        match c with
+        | bf_inc => bf_opt_inc 1
+        | bf_dec => bf_opt_dec 1
+        | bf_next => bf_opt_next 1
+        | bf_prev => bf_opt_prev 1
+        | bf_input => bf_opt_input
+        | bf_output => bf_opt_output
+        | bf_loop body =>
+            let optimized_body := bf_optimize body in
+            match optimized_body with
+            | bf_opt_cons (bf_opt_dec 1) bf_opt_nil => bf_opt_zero
+            | _ => bf_opt_loop optimized_body
+            end
+        end in
+      merge_command optimized_c (bf_optimize t)
+  end.
+
+
+(* Interpreting Optimized Programs *)
+
+Fixpoint add_cell (t : bf_tape) (n : nat) : bf_tape := match n with
+| O => t
+| S n' => add_cell (inc_cell t) n'
+end.
+
+Fixpoint sub_cell (t : bf_tape) (n : nat) : bf_tape := match n with
+| O => t
+| S n' => sub_cell (dec_cell t) n'
+end.
+
+Definition zero_cell (t : bf_tape) : bf_tape := {|
+  left := left t;
+  curr := O;
+  right := right t;
+|}.
+
+Fixpoint move_left (t : bf_tape) (n : nat) : bf_tape := match n with
+| O => t
+| S n' => move_left (shift_left t) n'
+end.
+
+Fixpoint move_right (t : bf_tape) (n : nat) : bf_tape := match n with
+| O => t
+| S n' => move_right (shift_right t) n'
+end.
+
+Fixpoint opt_interp (fuel : nat) (s : bf_state) (p : bf_opt_program) : option bf_state :=
+  match fuel, p with
+  | O, bf_opt_nil => Some s
+  | O, _ => None
+  | _, bf_opt_nil => Some s
+  | S (fuel'), bf_opt_cons h t => match opt_interp_single fuel' s h with
+    | Some s' => opt_interp fuel' s' t
+    | None => None
+    end
+  end
+
+with opt_interp_single (loop_fuel : nat) (s : bf_state) (c : bf_opt_command) : option bf_state :=
+  match c with
+  | bf_opt_inc n => Some {| tape := add_cell (tape s) n; input := input s; output := output s |}
+  | bf_opt_dec n => Some {| tape := sub_cell (tape s) n; input := input s; output := output s |}
+  | bf_opt_zero => Some {| tape := zero_cell (tape s); input := input s; output := output s |}
+  | bf_opt_next n => Some {| tape := move_right (tape s) n; input := input s; output := output s |}
+  | bf_opt_prev n => Some {| tape := move_left (tape s) n; input := input s; output := output s |}
+  | bf_opt_input => Some (input_cell s)
+  | bf_opt_output => Some (output_cell s)
+  | bf_opt_loop body => let fix loop (fuel : nat) (s : bf_state) := match fuel with
+    | O => None
+    | S (fuel') => if (curr (tape s)) =? 0 then Some s else
+      match opt_interp fuel' s body with
+      | None => None
+      | Some s' => loop fuel' s'
+      end
+    end
+    in loop loop_fuel s
+  end.
+
+Definition default_opt_interp := opt_interp default_fuel bf_default_state.
+
+(* Optimization tests *)
+
+Definition opt_is_equiv (p : bf_program) : Prop := default_interp p = default_opt_interp (bf_optimize p).
+
+Example opt_single_inc : opt_is_equiv <[ + !]>.
+Proof. reflexivity. Qed.
+
+Example opt_multi_inc : opt_is_equiv <[ + + + !]>.
+Proof. reflexivity. Qed.
+
+
+
 (* Theorems *)
 
 (* For any amount of fuel, there exists a program that will use it all up *)
@@ -153,7 +312,7 @@ Theorem fuel_necessary : forall (f : nat) (s : bf_state),
   exists (p : bf_program), interp f s p = None.
 Proof.
   intros. induction f.
-  - simpl. exists [bf_inc]. reflexivity.
+  - simpl. exists <[ + !]>. reflexivity.
   - simpl. admit.
 Admitted.
 
@@ -178,6 +337,5 @@ Theorem cell_max : forall (p : bf_program) (s : bf_state),
 Proof.
   intros. induction p.
   - simpl in H. apply option_equality in H. rewrite <- H. simpl. apply le_0_n.
-  - intros. induction a.
+  - intros. induction s.
 Admitted. (* DO NOT try `simpl in H` *)
-
